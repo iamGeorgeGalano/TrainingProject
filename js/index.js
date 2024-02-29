526654789bc139859bdb7a05b3cc6dfdb077a777
George Louis Galano <georgegalano@kalamansistudios.com>
2024-02-29 09:30:41 +0800

    TrainingProject

--- js/index.js
+++ js/index.js
@@ -1,3 +1,243 @@
+
+$(document).ready(function(){
+    var $input = $('#username');
+
+    $input.on("input", function(){
+        if ($input.val().length > 10){
+            $input.val($input.val().slice(0,10));
+        }
+    });
+
+    $('#suLink').on('click',function(){
+        window.location.href = "signup.html";
+    });
+
+    $('#logLink').on('click',function(){
+        window.location.href = "login.html";
+    });
+
+    $('#fgLink').on('click',function(){
+        window.location.href = "fgpass.html";
+    });
+
+    $('#fgUsername').on('click', function(){
+        window.location.href = "changeusername.html";
+    });
+
+    $('#sgSubmit').on('click', function(){
+        
+        function validateEmail(email){
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var email = $('#email').val();
+        
+        var exData = localStorage.getItem('dataList');
+        var exUser = exData ? JSON.parse(exData):[ ];
+        var emailExist = exUser.some(function(user){
+            return user.email == email;
+        });
+
+        if (username.trim() === "" && password.trim() === "" && email.trim() == ""){
+            $('#usernameError').text('Please fill up this field.');
+            $('#passwordError').text('Please fill up this field.');
+            $('#emailError').text('Please fill up this field.');
+        }else{
+            $('#usernameError').text('');
+            $('#passwordError').text('');
+            $('#emailError').text('');
+        }
+
+        if (username.trim()===""){
+            $('#usernameError').text('Please fill up this field.');
+        }else if (!/^[a-zA-Z0-9_]+$/.test(username)){
+            $('#usernameError').text('Username must only consists of letters, numbers, and underscore only')
+        }else if(password.trim()===""){
+            $('#passwordError').text('Please fill up this field');
+        }else if (email.trim() === ""){
+            $('#emailError').text('Please fill up this field.');
+        }else if(!validateEmail(email)){
+            $('#emailError').text('Please input valid email address');
+        }else if (emailExist){
+            $('#emailError').text('Email exists, use another email.')
+        }else {
+            $('#usernameError').text('')
+            $('#passwordError').text('')
+            $('#emailError').text('')
+
+            var userData = {
+                username: username,
+                password: password,
+                email: email
+            }
+
+            exUser.push(userData);
+
+            localStorage.setItem('dataList', JSON.stringify(exUser));
+    
+    
+           window.location.href = "login.html";
+             alert ("Data saved successfully");
+        };
+
+    });
+
+
+    $('#liSubmit').on('click', function(){
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var userList = JSON.parse(localStorage.getItem('dataList'));
+
+        if (!userList || userList.length === 0){
+            alert ("No user data found, sign up first");
+            return;
+        }
+
+        var userFound = false;
+
+        for (var i = 0; i < userList.length; i++){
+            var userData = userList[i];
+
+            if (userData.username === username && userData.password === password){
+                userFound = true;
+                $('#usernameError').text('');
+                $('#passwordError').text('');
+                alert("Login Successfully!!");
+            }
+        }
+        
+
+        if(!userFound){
+            if (username.trim() === "" && password.trim() === ""){
+                $('#usernameError').text('Username is required');
+                $('#passwordError').text('Password is required');
+                return;
+            }else if (username.trim() === ""){
+                $('usernameError').text('Username is required.');
+                return;
+            } else if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+                $('#usernameError').text('You input invalid content, Try again.');
+                return;
+            }  else if (password.trim() === ""){
+                $('#passwordError').text('Password is required');
+            }else{
+                $('#usernameError').text('');
+                $('#passwordError').text('');
+                alert("Invalid credentials, please try again.");
+            } 
+        } 
+   });
+
+    var usernameError = $('#usernameError');
+    var newPasswordInput = $('#password');
+    var confirmPasswordInput = $('#confirmPassword');
+    var submitButton = $('#fgButton');
+
+    $('#unChecker').on('click',function(){
+        function usernameValidator(validUsername){
+            var storedData = JSON.parse(localStorage.getItem('dataList'));
+
+            for (var i = 0; i < storedData.length; i++){
+                var userData = storedData[i];
+
+               if (userData.username === validUsername){
+                return true;
+               }
+            }
+    
+           return false;
+        }
+
+        var username = $('#username').val();
+
+        if (usernameValidator(username)){
+            usernameError.text('Username is valid').css('color', 'green');
+            newPasswordInput.prop('disabled', false);
+            confirmPasswordInput.prop('disabled', false);
+            submitButton.prop('disabled', false);
+        }else{
+            usernameError.text('Username is not valid').css('color', 'red');
+            newPasswordInput.prop('disabled', true);
+            confirmPasswordInput.prop('disabled', true);
+            submitButton.prop('disabled', true);
+        }
+
+    });
+
+    $('#fgButton').on('click', function(){
+        var storedData = JSON.parse(localStorage.getItem('dataList'));
+
+        var userChange = $('#username').val();
+
+        var userIndex = storedData.findIndex(function(user){
+            return user.username === userChange;
+        });
+
+        var password = $('#password').val();
+        var confirmPassword = $('#confirmPassword').val();
+
+        if(password === ""){
+            $('#passwordError').text('Password is required');
+            return
+        }else{
+            $('#passwordError').text('');
+        }
+
+        if(password != confirmPassword){
+                $('#passwordError1').text('Password not match'); 
+        }else{
+            
+            storedData[userIndex].password = password;
+            localStorage.setItem('dataList', JSON.stringify(storedData));
+            window.location.href = "login.html";
+            alert("Password changed successfully!!");
+        }
+
+    });
+
+    $('#fgUnButton').on('click', function(){
+        $('#UsernameError').text('');
+        $('#UsernameError1').text('');
+
+        var storedData = JSON.parse(localStorage.getItem('dataList'));
+
+        var emailChange = $('#email').val();
+
+        var userIndex = storedData.findIndex(function(user){
+            return user.email === emailChange;
+        });
+
+        var username = $('#username').val();
+        var confirmUsername = $('#confirmUsername').val();
+
+        if(username === ""){
+            $('#usernameError').text('Username is required');
+            return
+        }else if (!/^[a-zA-Z0-9_]+$/.test(username)){
+            $('#usernameError').text('Username must only consists of letters, numbers, and underscore only')
+        }else{
+            $('#UsernameError').text('');
+        }
+
+        if(username != confirmUsername){
+                $('#usernameError1').text('Usernames not match'); 
+        }else{
+            $('#UsernameError').text('');
+            $('#UsernameError1').text('');
+            storedData[userIndex].username = username;
+            localStorage.setItem('dataList', JSON.stringify(storedData));
+            window.location.href = "login.html";
+            alert("Username changed successfully!!");
+        }
+
+    });
+    
+});
+
+
 // var input =document.getElementById("username");
 
 // input.addEventListener("input", function() {
@@ -9,49 +249,22 @@
 
 // });
 
-$(document).ready(function(){
-    var $input = $('#username');
-
-    $input.on("input", function(){
-        if ($input.val().length > 10){
-            $input.val($input.val().slice(0,10));
-        }
-    });
-});
-
 // function goToSuPage() {
 //     window.location.href = "signup.html";
 // }
 
-$(document).ready(function(){
-    $('#suLink').on('click',function(){
-        window.location.href = "signup.html";
-    });
-});
+
 
 // function goToLoginPage() {
 //     window.location.href = "login.html";
 // }
 
-$(document).ready(function(){
-    $('#logLink').on('click',function(){
-        window.location.href = "login.html";
-    });;
-});
+
 
 // function goToFgPage(){
 //     window.location.href = "fgpass.html";
 // }
 
-$(document).ready(function(){
-    $('#fgLink').on('click',function(){
-        window.location.href = "fgpass.html";
-    });
-
-    $('#fgUsername').on('click', function(){
-        window.location.href = "changepass.html";
-    })
-});
 
 
 
@@ -126,68 +339,7 @@ $(document).ready(function(){
 //     alert("Data save successfully!;")
 // }
 
-$(document).ready(function(){
-    $('#sgSubmit').on('click', function(){
-        
-        function validateEmail(email){
-            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            return emailRegex.test(email);
-        }
 
-        var username = $('#username').val();
-        var password = $('#password').val();
-        var email = $('#email').val();
-        
-        var exData = localStorage.getItem('dataList');
-        var exUser = exData ? JSON.parse(exData):[ ];
-        var emailExist = exUser.some(function(user){
-            return user.email == email;
-        });
-
-        if (username.trim() === "" && password.trim() === "" && email.trim() == ""){
-            $('#usernameError').text('Please fill up this field.');
-            $('#passwordError').text('Please fill up this field.');
-            $('#emailError').text('Please fill up this field.');
-        }else{
-            $('#usernameError').text('');
-            $('#passwordError').text('');
-            $('#emailError').text('');
-        }
-
-        if (username.trim()===""){
-            $('#usernameError').text('Please fill up this field.');
-        }else if (!/^[a-zA-Z0-9_]+$/.test(username)){
-            $('#usernameError').text('Username must only consists of letters, numbers, and underscore only')
-        }else if(password.trim()===""){
-            $('#passwordError').text('Please fill up this field');
-        }else if (email.trim() === ""){
-            $('#emailError').text('Please fill up this field.');
-        }else if(!validateEmail(email)){
-            $('#emailError').text('Please input valid email address');
-        }else if (emailExist){
-            $('#emailError').text('Email exists, use another email.')
-        }else {
-            $('#usernameError').text('')
-            $('#passwordError').text('')
-            $('#emailError').text('')
-
-            var userData = {
-                username: username,
-                password: password,
-                email: email
-            }
-
-            exUser.push(userData);
-
-            localStorage.setItem('dataList', JSON.stringify(exUser));
-    
-    
-           window.location.href = "login.html";
-             alert ("Data saved successfully");
-        };
-
-    });
-});
 
 // function validateLogin (){
 
@@ -226,53 +378,7 @@ $(document).ready(function(){
         
 //     }
 // }
-
-$(document).ready(function(){
-    $('#liSubmit').on('click', function(){
-        var username = $('#username').val();
-        var password = $('#password').val();
-        var userList = JSON.parse(localStorage.getItem('dataList'));
-
-        if (!userList || userList.length === 0){
-            alert ("No user data found, sign up first");
-            return;
-        }
-
-        var userFound = false;
-
-        for (var i = 0; i < userList.length; i++){
-            var userData = userList[i];
-
-            if (userData.username === username && userData.password === password){
-                userFound = true;
-                $('#usernameError').text('');
-                $('#passwordError').text('');
-                alert("Login Successfully!!");
-            }
-        }
-        
-
-        if(!userFound){
-            if (username.trim() === "" && password.trim() === ""){
-                $('#usernameError').text('Username is required');
-                $('#passwordError').text('Password is required');
-                return;
-            }else if (username.trim() === ""){
-                $('usernameError').text('Username is required.');
-                return;
-            } else if (!/^[a-zA-Z0-9_]+$/.test(username)) {
-                $('#usernameError').text('You input invalid content, Try again.');
-                return;
-            }  else if (password.trim() === ""){
-                $('#passwordError').text('Password is required');
-            }else{
-                $('#usernameError').text('');
-                $('#passwordError').text('');
-                alert("Invalid credentials, please try again.");
-            } 
-        } 
-   });
-}); 
+ 
 
 // function usernameValidator(validUsername){
 
@@ -291,83 +397,8 @@ $(document).ready(function(){
 //     }
 // });
 
-$(document).ready(function(){
 
-    var usernameError = $('#usernameError');
-    var newPasswordInput = $('#password');
-    var confirmPasswordInput = $('#confirmPassword');
-    var submitButton = $('#fgButton');
 
-    $('#unChecker').on('click',function(){
-        function usernameValidator(validUsername){
-            var storedData = JSON.parse(localStorage.getItem('dataList'));
-
-            for (var i = 0; i < storedData.length; i++){
-                var userData = storedData[i];
-
-               if (userData.username === validUsername){
-                return true;
-               }
-            }
-    
-           return false;
-        }
-
-        var username = $('#username').val();
-
-        if (usernameValidator(username)){
-            usernameError.text('Username is valid').css('color', 'green');
-            newPasswordInput.prop('disabled', false);
-            confirmPasswordInput.prop('disabled', false);
-            submitButton.prop('disabled', false);
-        }else{
-            usernameError.text('Username is not valid').css('color', 'red');
-            newPasswordInput.prop('disabled', true);
-            confirmPasswordInput.prop('disabled', true);
-            submitButton.prop('disabled', true);
-        }
-
-    });
-});
-
-$(document).ready(function(){
-
-    var emailError = $('#emailError');
-    var newUsernameInput = $('#username');
-    var confirmUsernameInput = $('#confirmUsername');
-    var submitButton = $('#fgUnButton');
-
-    $('#emChecker').on('click',function(){
-        function usernameValidator(validEmail){
-            var storedData = JSON.parse(localStorage.getItem('dataList'));
-
-            for (var i = 0; i < storedData.length; i++){
-                var userData = storedData[i];
-
-               if (userData.email === validEmail){
-                return true;
-               }
-            }
-    
-           return false;
-        }
-
-        var email = $('#email').val();
-
-        if (usernameValidator(email)){
-            emailError.text('Username is valid').css('color', 'green');
-            newUsernameInput.prop('disabled', false);
-            confirmUsernameInput.prop('disabled', false);
-            submitButton.prop('disabled', false);
-        }else{
-            emailError.text('Username is not valid').css('color', 'red');
-            newUsernameInput.prop('disabled', true);
-            confirmUsernameInput.prop('disabled', true);
-            submitButton.prop('disabled', true);
-        }
-
-    });
-});
 
 // function fgPasswordCondition() {
 
@@ -393,77 +424,7 @@ $(document).ready(function(){
 // }
 
 
-$(document).ready(function(){
-    $('#fgButton').on('click', function(){
-        var storedData = JSON.parse(localStorage.getItem('dataList'));
 
-        var userChange = $('#username').val();
-
-        var userIndex = storedData.findIndex(function(user){
-            return user.username === userChange;
-        });
-
-        var password = $('#password').val();
-        var confirmPassword = $('#confirmPassword').val();
-
-        if(password === ""){
-            $('#passwordError').text('Password is required');
-            return
-        }else{
-            $('#passwordError').text('');
-        }
-
-        if(password != confirmPassword){
-                $('#passwordError1').text('Password not match'); 
-        }else{
-            
-            storedData[userIndex].password = password;
-            localStorage.setItem('dataList', JSON.stringify(storedData));
-            window.location.href = "login.html";
-            alert("Password changed successfully!!");
-        }
-
-    });
-});
-
-$(document).ready(function(){
-    $('#fgUnButton').on('click', function(){
-        $('#UsernameError').text('');
-        $('#UsernameError1').text('');
-
-        var storedData = JSON.parse(localStorage.getItem('dataList'));
-
-        var emailChange = $('#email').val();
-
-        var userIndex = storedData.findIndex(function(user){
-            return user.email === emailChange;
-        });
-
-        var username = $('#username').val();
-        var confirmUsername = $('#confirmUsername').val();
-
-        if(username === ""){
-            $('#usernameError').text('Username is required');
-            return
-        }else if (!/^[a-zA-Z0-9_]+$/.test(username)){
-            $('#usernameError').text('Username must only consists of letters, numbers, and underscore only')
-        }else{
-            $('#UsernameError').text('');
-        }
-
-        if(username != confirmUsername){
-                $('#usernameError1').text('Usernames not match'); 
-        }else{
-            $('#UsernameError').text('');
-            $('#UsernameError1').text('');
-            storedData[userIndex].username = username;
-            localStorage.setItem('dataList', JSON.stringify(storedData));
-            window.location.href = "login.html";
-            alert("Username changed successfully!!");
-        }
-
-    });
-});
 
 // function changePassword () {
     
